363b0fe07813d950665858944cc46748230b0f6a
jimitas <nonajun805@gmail.com>
2023-01-07 06:20:17 +0900

    src フォルダの作成 pages stylesの移動

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,8 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-
+        <h1>地味に助かる　作戦ボード</h1>
       </main>
     </>
-  )
+  );
 }
