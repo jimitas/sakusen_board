d7fc38f0b5116fb2376b137a2814d8a7ae90d6e7
jimitas <nonajun805@gmail.com>
2023-01-07 11:52:53 +0900

    kansei

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import Draggable from "react-draggable";
 import { useState } from "react";
 export default function Home() {
   const NUM = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const ARROW_PINK = [1, 1, 2, 2, 3, 3, 4, 4];
+  const ARROW_BLUE = [5, 5, 6, 6, 7, 7, 8, 8];
   const SAKUSEN = [
     "チームの作戦名は？",
     "ガンガン行こうぜ！",
@@ -23,9 +25,9 @@ export default function Home() {
 
   return (
     <>
-      <body className="text-white bg-yellow-900 h-screen select-none">
+      <body className="text-white bg-green-700 h-screen select-none">
         <header style={{ height: "10vh", borderBottom: "1px white solid" }} className="p-3 flex justify-center">
-          <div className="text-3xl">地味に助かる　作戦ボード</div>
+          <div className="text-4xl">地味に助かる　作戦ボード</div>
           <div>
             <select className="select mx-3 -mt-1">
               {SAKUSEN.map((item) => {
@@ -67,21 +69,54 @@ export default function Home() {
               );
             })}
           </section>
-          <div>
-            <Draggable defaultPosition={{ x: 0, y: 0 }}>
-              <img style={{ width: "50px", height: "50px",cursor: "pointer" }} src="/ball.png" alt="" />
-            </Draggable>
-            <button
-            className="btn mt-3"
-            onClick={() => {
-              setIsShow((isShow) => {
-                return !isShow;
-              });
-            }}
-          >
-            {isShow ? "戻す" : "回転"}
-          </button>
-          </div>
+
+          <section style={{ width: "260px" }} className="flex flex-wrap">
+            {ARROW_PINK.map((item) => {
+              return (
+                <Draggable defaultPosition={{ x: 0, y: 0 }}>
+                  <img
+                    key={item}
+                    style={{ width: "50px", height: "50px", cursor: "pointer" }}
+                    src={`/arrow_${item}.png`}
+                    alt=""
+                  />
+                </Draggable>
+              );
+            })}
+          </section>
+
+          <section>
+            <div>
+              <Draggable defaultPosition={{ x: 0, y: 0 }}>
+                <img style={{ width: "50px", height: "50px", cursor: "pointer" }} src="/ball.png" alt="" />
+              </Draggable>
+              <button
+                className="btn mt-3"
+                onClick={() => {
+                  setIsShow((isShow) => {
+                    return !isShow;
+                  });
+                }}
+              >
+                {isShow ? "戻す" : "回転"}
+              </button>
+            </div>
+          </section>
+
+          <section style={{ width: "260px" }} className="flex flex-wrap">
+            {ARROW_BLUE.map((item) => {
+              return (
+                <Draggable defaultPosition={{ x: 0, y: 0 }}>
+                  <img
+                    key={item}
+                    style={{ width: "50px", height: "50px", cursor: "pointer" }}
+                    src={`/arrow_${item}.png`}
+                    alt=""
+                  />
+                </Draggable>
+              );
+            })}
+          </section>
 
           <section style={{ width: "260px" }} className="flex  flex-wrap mx-auto">
             {NUM.map((num) => {
