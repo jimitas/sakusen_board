8698f21b0ad3b6f844090ed1f1b86948ba760d8d
jimitas <nonajun805@gmail.com>
2023-01-07 10:30:00 +0900

    ユーザーセレクトnoneなど細かい修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,40 @@ import Draggable from "react-draggable";
 
 export default function Home() {
   const NUM = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const SAKUSEN = [
+    "チームの作戦名は？",
+    "ガンガン行こうぜ！",
+    "バッチリがんばれ！",
+    "わたしにまかせて！",
+    "守りを大事に！",
+    "つないで行こう",
+    "声をかけあって行こう",
+    "指示を守って行こう",
+    "マークして行こう",
+    "落ち着いて行こう",
+    "はげましあって行こう",
+    "あきらめないで行こう！",
+    "作戦A",
+    "作戦B",
+    "作戦C",
+  ];
   return (
     <>
-      <body className="text-white bg-yellow-900 h-screen">
+      <body className="text-white bg-yellow-900 h-screen select-none">
         <header style={{ height: "10vh", borderBottom: "1px white solid" }} className="p-3 flex justify-center">
-          <div className="text-3xl">地味に助かる　作戦ボード　</div>
+          <div className="text-3xl">地味に助かる　作戦ボード</div>
+          <div>
+            <select className="select mx-3 -mt-1">
+              {SAKUSEN.map((item) => {
+                return (
+                  <option key={item} className="cursor-pointer" value={item}>
+                    {item}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="pt-3">スクリーンショットをとって、ロイロノートで編集すると役立つかも。</div>
         </header>
 
         <main style={{ height: "70vh" }}>
