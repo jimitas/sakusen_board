bd84a3a0b576051c7c6ec2a8576e5ea4adc56cf4
jimitas <nonajun805@gmail.com>
2023-01-07 10:02:47 +0900

    基礎完成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,51 @@
-// import styles from "../styles/Home.module.css";
-
+import Draggable from "react-draggable";
 
 export default function Home() {
-  const ITEM = ["サッカー", "バレーボール", "ラグビー", "バスケットボール"];
-  const NUM = [3, 4, 5, 6, 7, 8, 9];
+  const NUM = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       <body className="text-white bg-yellow-900 h-screen">
-        <header className="p-3 flex justify-center">
-          <div className="text-5xl">地味に助かる　作戦ボード　</div>
-
-        
-
+        <header style={{ height: "10vh", borderBottom: "1px white solid" }} className="p-3 flex justify-center">
+          <div className="text-3xl">地味に助かる　作戦ボード　</div>
         </header>
-        <main>
 
+        <main style={{ height: "70vh" }}>
+          <section className="couat mx-auto mt-5">
+            <div className="absolute left_goal"></div>
+            <div className="absolute left_line"></div>
+            <div className="absolute mid_line"></div>
+            <div className="absolute right_line"></div>
+            <div className="absolute right_goal"></div>
+          </section>
         </main>
+        <fotter style={{ height: "10vh" }} className="flex">
+          <section style={{ width: "260px" }} className="flex flex-wrap mx-auto">
+            {NUM.map((num) => {
+              return (
+                <Draggable defaultPosition={{ x: 0, y: 0 }}>
+                  <div key={num} className="red cursor-pointer" value={num}>
+                    {num}
+                  </div>
+                </Draggable>
+              );
+            })}
+          </section>
+          <div>
+            <Draggable defaultPosition={{ x: 0, y: 0 }}>
+              <img style={{ width: "50px", height: "50px", cursor: "pointer" }} src="/ball.png" alt="" />
+            </Draggable>
+          </div>
+
+          <section style={{ width: "260px" }} className="flex flex-wrap mx-auto">
+            {NUM.map((num) => {
+              return (
+                <div key={num} className="blue cursor-pointer" value={num}>
+                  {num}
+                </div>
+              );
+            })}
+          </section>
+        </fotter>
       </body>
     </>
   );
